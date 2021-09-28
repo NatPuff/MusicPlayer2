30567f2844a6a4678ae83bda2191a8caad61937e
NatPuff <cesarp102104@gmail.com>
2021-09-28 08:36:11 -0500

    Initial Commit

---
+++ MusicPlayer2/MusicPlayer2App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayer2App.swift
+//  MusicPlayer2
+//
+//  Created by AMStudent on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
