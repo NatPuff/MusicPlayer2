e47fd9c3a58b9d494020fc8288a5bc3b47a9353a
NatPuff <cesarp102104@gmail.com>
2021-10-08 10:31:50 -0500

    ewe
    finished

--- MusicPlayer2/ContentView.swift
+++ MusicPlayer2/ContentView.swift
@@ -5,13 +5,79 @@
 //  Created by AMStudent on 9/28/21.
 //
 
+
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var audioPlayer: AVAudioPlayer!
+    @State var song = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color("AccentColor").ignoresSafeArea()
+
+        VStack{
+            HStack{
+        Text("Music Player")
+            
+            .font(.system(size: 45))
+            .fontWeight(.bold)
+            .multilineTextAlignment(.center)
+            .foregroundColor(Color("Bruh"))
+            }
+            Image("Neonix")
+                .resizable()
+                .frame(width: 200, height: 200)
+            HStack{
+                Button(action: {
+                    audioPlayer.play()
+                }) {
+                    Image("play")
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(Color("Bruh"))
+                        .padding(.top)
+                }
+                Button(action: {
+                    audioPlayer.pause()
+                }) {
+                    Image("pause")
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(Color("Bruh"))
+                        .padding(.top)
+               }
+                Button(action: {
+                    if self.song < 3 {
+                        self.song += 1
+                            
+                        } else {
+                            self.song = 1
+                        }
+                        let sound = Bundle.main.path(forResource: "song\(self.song)", ofType: "mp3")
+                        self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+                        self.audioPlayer.play()
+                    
+                    
+                })  {
+                    Image("next")
+                        .renderingMode(.template)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color("Bruh"))
+                        .padding(.top)
+                    
+                }
+            }
+        }
+        .onAppear {
+        let sound = Bundle.main.path(forResource: "song1", ofType: "mp3")
+            audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+       }
     }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +85,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
